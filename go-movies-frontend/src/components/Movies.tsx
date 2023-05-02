3d3f3e1189aa97b3635f9a8a51a5987229607f50
shitakemura <shitakemura@gmail.com>
2023-05-02 10:23:22 +0900

    add react proxy settings by vite

--- go-movies-frontend/src/components/Movies.tsx
+++ go-movies-frontend/src/components/Movies.tsx
@@ -16,10 +16,7 @@ function Movies() {
       }
 
       try {
-        const response = await fetch(
-          `http://localhost:8080/movies`,
-          requestOptions,
-        )
+        const response = await fetch(`api/movies`, requestOptions)
         const data = await response.json()
         setMovies(data)
       } catch (err) {
--- go-movies-frontend/vite.config.ts
+++ go-movies-frontend/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // https://ja.vitejs.dev/config/server-options.html#server-proxy
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 })
