022e65b062aaf95108162049e2bb525335bf5572
shitakemura <shitakemura@gmail.com>
2023-05-08 00:08:16 +0900

    add handler and db method to delete a movie

--- go-movies-backend/internal/repository/repository.go
+++ go-movies-backend/internal/repository/repository.go
@@ -16,4 +16,5 @@ type DatabaseRepo interface {
 	InsertMovie(movie models.Movie) (int, error)
 	UpdateMovieGenres(id int, genreIds []int) error
 	UpdateMovie(movie models.Movie) error
+	DeleteMovie(id int) error
 }
